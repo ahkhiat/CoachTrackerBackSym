26dbde29434e8de51bac2a4d279bddf925fb0b2b
thierryleung <ahkhiat@hotmail.com>
2025-02-23 11:34:54 +0100

    autentification activé

---
+++ src/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+final class UserController extends AbstractController
+{
+    #[Route('/api/user/profile', name: 'user_profile', methods:['GET'])]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
